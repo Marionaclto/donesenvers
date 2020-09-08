00a9c12df35c9014d49eb0ffc831be19c2589af0
mariona clarà torroglosa <mariona.clto@gmail.com>
2020-09-08 17:41:48 +0200

    react-router and home

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route, NavLink} from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
 
@@ -9,7 +10,11 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        
+        <Router>
+          <Switch>
+            <Route exact path='/' component={HomePage}></Route>
+          </Switch>
+        </Router>
       </div>
     );
   }  
